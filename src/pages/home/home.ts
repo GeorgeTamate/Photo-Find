e5b2c1fccdef450455d4c3712702384c6980ee25
GeorgeTamate <georgetamate@gmail.com>
2016-11-21 13:44:09 -0400

    Sends a POST request to the Server with a Base64 encoded picture, taken from the camera, as a x-www-form-urlencoded type parameter.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -40,12 +40,31 @@ export class HomePage {
       });
   }
 
+  makeRequestToServer() {
+    this.httpApi.getToServer().subscribe(
+      data => {
+        console.log('success');
+        console.log('data', data.results);
+        this.people = data.results;
+      },
+      err => {
+        // Uh Oh
+        console.log('err', err);
+      },
+      () => {
+        console.log('complete');
+      });
+    console.log('HEY! PUCMM Server!!!');
+  }
+
   takePicture() {
     // getPicture options
     let options = {
       quality: 75,
-      destinationType: Camera.DestinationType.FILE_URI,
-      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      //destinationType: Camera.DestinationType.FILE_URI,
+      destinationType: Camera.DestinationType.DATA_URL,
+      //sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      sourceType: Camera.PictureSourceType.CAMERA,
       allowEdit: true,
       encodingType: Camera.EncodingType.JPEG,
       saveToPhotoAlbum: false
@@ -54,9 +73,17 @@ export class HomePage {
     Camera.getPicture(options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
-      // this.base64Image = "data:image/jpeg;base64," + imageData;
-      this.base64Image = imageData;
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+      // this.base64Image = imageData;
+      this.makeRequestToServer();
       console.log('imgData', imageData);
+      
+      this.httpApi.post(imageData).subscribe(
+        data => { console.log('POSTing on Server'); },
+        err => {console.log('err', err);},
+        () => { console.log('POSTed on Server'); }
+      );
+
     }, (err) => {
       // Handle error
       console.log('err', err);
--- src/shared/services/simplehttp.service.ts
+++ src/shared/services/simplehttp.service.ts
@@ -1,6 +1,6 @@
 // Angular
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 
 // 3rd parties
 import 'rxjs';
@@ -16,4 +16,23 @@ export class SimpleHttp {
         return response.json();
       });
   }
+
+  getToServer() {
+    return this.http.get('http://8afba4a9.ngrok.io')
+      .map((response: Response) => {
+        return response.json();
+      });
+  }
+
+  post(data) {
+    var params = 'pic=' + data;
+  	var headers = new Headers();
+  	headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this.http.post('http://8afba4a9.ngrok.io', params, {headers: headers})
+      .map((response: Response) => {
+        return response.json();
+      });
+
+  } 
 }
